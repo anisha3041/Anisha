59cc64c23449b8affc77f4a69e48e0397ca8b025
anisha3041 <167450114+anisha3041@users.noreply.github.com>
2025-05-03 12:42:50 +0530

    loop

---
+++ 14.py
@@ -0,0 +1,13 @@
+#using for loop with break
+'''mydata =[1,2,3,4,5,6,7,8]
+for i in mydata:
+    if i== 3:
+        continue
+    print(i)'''
+
+#
+mydata =[1,2,3,4,5,6,7,8]
+for i in mydata:
+      print(i)
+      if i == 3:
+           continue
